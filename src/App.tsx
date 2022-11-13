b34dea68de7bfd7c3ab70127c1730a66a37ca96d
nghiapt-comatek <100978931+jenazu@users.noreply.github.com>
2022-11-13 09:59:09 +0700

    build

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import React from "react";
+import styled from "styled-components";
 
 import "./App.css";
 
+
 function App() {
-  return <div className="App">hello</div>;
+  return <Container>hello</Container>;
 }
 
+
 export default App;
+
+const Container = styled.div`
+  background: #fff;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+`;
